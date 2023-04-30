23543c380b5ec59c6672b4119553cc5d302c6c93
AmanSingh <amansinghr884@gmail.com>
2023-04-30 17:46:35 +0530

    Buy ticket linked with url

--- app/src/main/java/com/ticket/master/eventfinder/eventTabs/DetailsFragment.kt
+++ app/src/main/java/com/ticket/master/eventfinder/eventTabs/DetailsFragment.kt
@@ -1,6 +1,12 @@
 package com.ticket.master.eventfinder.eventTabs
 
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify.WEB_URLS
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -36,6 +42,7 @@ class DetailsFragment : Fragment() {
 
         binding.artistTeamsTxt.isSelected = true
         binding.buyTicketAtText.isSelected = true
+        binding.buyTicketAtText.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
         viewModel.uiState.observe(viewLifecycleOwner) {
             when (it) {
@@ -51,29 +58,34 @@ class DetailsFragment : Fragment() {
     }
 
     private fun bind() {
-        viewModel.eventData.let {
-            binding.artistTeamsTxt.text = it.name
-            binding.venueTxt.text = it._embedded.venues[0].name
+        viewModel.eventData.let {event->
+            binding.artistTeamsTxt.text = event.name
+            binding.venueTxt.text = event._embedded.venues[0].name
             val dateFormat = SimpleDateFormat("MMM dd,yyyy")
-            binding.dateTxt.text = dateFormat.format(it.dates.start.date)
+            binding.dateTxt.text = dateFormat.format(event.dates.start.date)
             val timeFormat = SimpleDateFormat("h:mm a")
-            binding.timeTxt.text = timeFormat.format(it.dates.start.time)
-            if (it.url != null) {
-                binding.buyTicketAtText.text = it.url
+            binding.timeTxt.text = timeFormat.format(event.dates.start.time)
+            if (event.url != null) {
+                binding.buyTicketAtText.text = event.url
+                binding.buyTicketAtText.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse(event.url)
+                    startActivity(intent)
+                }
             } else {
                 binding.buyTicketAtText.visibility = View.GONE
                 binding.buyTicket.visibility = View.GONE
             }
 
-            if (it.priceRanges != null) {
+            if (event.priceRanges != null) {
                 binding.priceRangeTxt.text =
-                    it.priceRanges[0].min.toString() + " - " + it.priceRanges[0].max.toString() + " (" + it.priceRanges[0].currency + ")"
+                    event.priceRanges[0].min.toString() + " - " + event.priceRanges[0].max.toString() + " (" + event.priceRanges[0].currency + ")"
             } else {
                 binding.priceRangeTxt.visibility = View.GONE
                 binding.priceRange.visibility = View.GONE
             }
             Glide.with(binding.seatImage.context)
-                .load(it.seatmap.staticUrl)
+                .load(event.seatmap.staticUrl)
                 .transition(DrawableTransitionOptions.withCrossFade())
                 .into(binding.seatImage)
         }
