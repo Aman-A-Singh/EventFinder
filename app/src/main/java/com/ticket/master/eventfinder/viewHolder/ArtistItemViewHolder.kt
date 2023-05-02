c105e12331cc6bf9f1319c12c89f9181d1eeba13
AmanSingh <amansinghr884@gmail.com>
2023-05-02 23:11:27 +0530

    Albums row created

--- app/src/main/java/com/ticket/master/eventfinder/viewHolder/ArtistItemViewHolder.kt
+++ app/src/main/java/com/ticket/master/eventfinder/viewHolder/ArtistItemViewHolder.kt
@@ -2,11 +2,13 @@ package com.ticket.master.eventfinder.viewHolder
 
 import android.content.Intent
 import android.net.Uri
+import android.view.View
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.ticket.master.eventfinder.databinding.ArtistItemRowBinding
+import com.ticket.master.eventfinder.models.artist.Album
 import com.ticket.master.eventfinder.models.artist.ArtistDetail
 
 class ArtistItemViewHolder(private val itemBinding: ArtistItemRowBinding) :
@@ -29,5 +31,26 @@ class ArtistItemViewHolder(private val itemBinding: ArtistItemRowBinding) :
 
         itemBinding.popularityProgress.progress = item.popularity
         itemBinding.popularityValueTxt.text = item.popularity.toString()
+
+        configureAlbum(item.albums)
+    }
+
+    private fun configureAlbum(albums: List<Album>) {
+        if (albums.size != 0) {
+            Glide.with(itemBinding.album1Image.context)
+                .load(albums[0].images[0].url)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(itemBinding.album1Image)
+            Glide.with(itemBinding.album2Image.context)
+                .load(albums[1].images[0].url)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(itemBinding.album2Image)
+            Glide.with(itemBinding.album3Image.context)
+                .load(albums[2].images[0].url)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(itemBinding.album3Image)
+        } else {
+            itemBinding.albumCardView.visibility = View.GONE
+        }
     }
 }
